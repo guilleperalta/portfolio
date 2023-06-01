aadae4f1c6e6ae29c3a0ed0bebdad4f5b8d3638f
Guille Peralta <guille.peralta.dev@gmail.com>
2023-06-01 11:31:33 -0300

    Se fixea el scroll

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     section.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
+        let offset = sec.offsetTop - 400;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
